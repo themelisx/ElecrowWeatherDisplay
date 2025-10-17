8e4c6ece9c637d0bcbdd4f1c8b87c2110122727e
Christos Themelis <themelisx@gmail.com>
2025-10-17 20:10:23 +0300

    Create user_setup.h
    This file is in git ignore. Your data will stay local

---
+++ Code/include/user_setup.h
@@ -0,0 +1,15 @@
+///////////////////////////////////////////////////////////
+// This file is in git ignore. Your data will stay local //
+///////////////////////////////////////////////////////////
+
+//#################### EDIT THIS  ###################
+// WiFi
+#define USER_WiFiSSID "MyWiFiSSID"
+#define USER_WiFiPassword "MyWiFiPassword
+// Clock
+#define USER_TimeZone 2
+// Open Weather
+#define USER_WeatherTown "Thessaloniki"
+#define USER_WeatherAPI "000000000000000000000000000000"
+#define USER_WeatherUnits "metric"
+//#################### end of edits ###################
